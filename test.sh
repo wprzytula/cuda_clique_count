91bc2da3fadc83fd62384eb05e3a0414d7da8102
Wojciech Przytuła <wp418383@students.mimuw.edu.pl>
2023-05-07 13:46:21 +0200

    test.sh: add Bartek's

--- test.sh
+++ test.sh
@@ -1,6 +1,14 @@
-cd tests/poprawnosciowe_dominik
-for test in `ls input`; do
-    echo -e "\n	Running test ${test}..."
-    ../../kcliques input/$test 12 tmp.file
-    diff tmp.file output/$test
+#!/bin/zsh
+
+declare -A k
+k[poprawnosciowe_dominik]=12
+k[bartka_tests]=11
+
+cd tests
+for testdir in `ls`; do
+    [ -d $testdir ] && for testname in $(ls "${testdir}/input"); do
+        echo -e "\n	Running test ${testname}..."
+        ../kcliques ${testdir}/input/$testname $k[${testdir}] tmp.file
+        diff tmp.file ${testdir}/output/$testname || exit 1
+    done
 done
